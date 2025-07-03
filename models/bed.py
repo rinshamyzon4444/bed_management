84d63bbe1d989e4789299885bc0abf4b18cf6f93
srisathyasidmec <srisathya@sidmectech.in>
2025-07-03 15:19:00 +0530

    changes

--- models/bed.py
+++ models/bed.py
@@ -3,7 +3,7 @@ from odoo import models, fields, api
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
-    is_bed = fields.Boolean(string="Is Bed Product", tracking=True, default=True)
+    is_bed = fields.Boolean(string="Is Bed Product", tracking=True, default=False)
     bed_type = fields.Selection([
         ('king', 'King'),
         ('queen', 'Queen'),
@@ -14,7 +14,10 @@ class ProductTemplate(models.Model):
 
     wood_type = fields.Char(string="Wood Type", tracking=True)
 
-
     detailed_type = fields.Selection(default='product')  # 'product' = Storable
     sale_ok = fields.Boolean(default=True)
     purchase_ok = fields.Boolean(default=True)
+
+    length = fields.Float(string="Length (cm)")
+    width = fields.Float(string="Width (cm)")
+    height = fields.Float(string="Height (cm)")
